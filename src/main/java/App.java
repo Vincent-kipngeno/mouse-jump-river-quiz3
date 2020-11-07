8173c126bf3fd3c5d025903a3d9731fec3bcfd4f
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-08 00:44:39 +0300

    Create App.java class

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+    private static  int possibleWays(int totalFeet) {
+        return 0;
+    }
+}
+
